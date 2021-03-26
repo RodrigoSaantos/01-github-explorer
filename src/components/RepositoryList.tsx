9a70491ce30fd250b2bf5137c372ebe1b4d6655b
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-03-25 22:48:31 -0300

    Components with typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,11 +1,15 @@
+import { useEffect, useState } from "react";
+import '../styles/repositories.scss';
 import { RepositoryItem } from "./RepositoryItem";
 
-import '../styles/repositories.scss';
-import { useEffect, useState } from "react";
-
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://github.com/orgsrocketseat/repos')
